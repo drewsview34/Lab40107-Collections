dec59b4f40460fc5072d61dd8b7a4f719b5cc74e
Andrew Hinojosa <drewsview34@gmail.com>
2019-01-16 23:07:49 -0800

    Add project files.

---
+++ Lab40107-Collections/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab40107_Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
